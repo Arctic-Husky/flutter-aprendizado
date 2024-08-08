80ee1f889cfd9e3306e3b4623659d0b72df86710
dadas <dadsada@dadas.com>
2024-08-08 17:35:08 -0300

    Altera readme com Widgets Importantes

--- README.md
+++ README.md
@@ -1,16 +1,80 @@
-# flutter_application
+# Flutter Aprendizado
 
-A new Flutter project.
+Não vou colocar todos os widgets que existem aqui, apenas alguns notáveis
 
-## Getting Started
+# Widgets Importantes
 
-This project is a starting point for a Flutter application.
+## Básicos
 
-A few resources to get you started if this is your first Flutter project:
+Scaffold
+- Permite que você implemente design visual nos widgets filhos
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+Container
+- Permite que você manipule cor, posicionamento e tamanho de widgets filhos
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Layout
+
+Padding
+- Permite que você adicione espaço em qualquer lado de widgets filhos. Padding usa EdgeInsets.
+- Muito útil para delimitar limites aos widgets, pois muitos deles são expansivos por padrão, como o ElevatedButton.
+
+Align
+- Permite que você posicione widgets filho dentro de um widget pai. É possível providenciar uma posição específica com ```Alignment(num, num)```
+
+ConstrainedBox
+- Permite que você especifique uma altura ou largura máxima ou mínima para o widget filho.
+- Caso de uso: Fazer com que um texto grande dentro de uma caixa se divida em dois com quebra de linha.
+
+FittedBox
+- Permite que você especifique como um widget filho vai se escalonar e posicionar dentro do widget pai usando o `BoxFit`.
+
+ListView
+
+GridView
+
+LayoutBuilder
+- Permite que você possa obter o valor do tamanho do espaço que o widget filho possui, contando com reduções de padding e outros.
+
+Wrap
+- Se o Row ou Column causarem overflow, este widget pode ser usado para organizar melhor uma lista de widgets em um espaço de maneira automática.
+
+## Slivers
+
+Os Slivers /não são Widgets, e eles/? devem ser usados dentro de um ScrollView.
+
+ListView is a SliverList. Same with GridView, which is a SliverGrid.
+They are doing exactly the same thing. The only difference between them is that SliverList is a sliver, not a widget. Which means it's used inside a ScrollView, usually CustomScrollView.
+ListView is nothing else but a biding of SliverList to transform it into a Widget to make it usable alongside other widgets such as Row/Container.
+
+Estranha essa fala pq SliverList herda de Widget. Porém o SliverList é mais capaz do que o ListView.
+
+## Styling
+
+## Painting
+
+ClipRect
+- Permite que você defina um retângulo, e tudo o que está fora do retângulo é cortado do widget filho.
+
+Opacity
+
+## Material
+
+Os widgets de Material são botões, cards, barras, inputs, interfaces variadas que implementam um design pre pronto do `Material 3`.
+
+Divider
+
+ListTile
+
+Card
+
+## Async
+
+FutureBuilder
+
+StreamBuilder
+
+## Animation
+
+Como decidir que widget ou builder usar pra fazer uma animação.
+
+![alt text](image.png)
--- lib/pages/testewidgetspage/testewidgetspage.dart
+++ lib/pages/testewidgetspage/testewidgetspage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class TesteWidgetsPage extends StatefulWidget {
   const TesteWidgetsPage({super.key});
@@ -8,6 +9,86 @@ class TesteWidgetsPage extends StatefulWidget {
 }
 
 class TesteWidgetsPageState extends State<TesteWidgetsPage> {
+  List<Widget> widgets = [
+    Container(
+      // Define o tamanho do Container
+      width: 80, // Ajuste os valores
+      height: 40, // Ajuste os valores
+      // Centraliza o ElevatedButton
+      alignment: Alignment.center,
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text("Botão"),
+      ),
+    ),
+    Container(
+      // Define o tamanho do Container
+      width: 80, // Ajuste os valores
+      height: 40, // Ajuste os valores
+      // Centraliza o ElevatedButton
+      alignment: Alignment.center,
+      child: const ElevatedButton(
+        onPressed: null,
+        child: Text("Botão"),
+      ),
+    ),
+    Container(
+      alignment: Alignment.center,
+      child: Stack(alignment: Alignment.center, children: [
+        Image.network(
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsa3K1PkfEgVzc6JeayE-uGwejpsBDBbVBUw&s"),
+        const Text("Laranjas"),
+      ]),
+    ),
+    Container(
+      alignment: Alignment.center,
+      child: Table(
+        border: TableBorder.all(),
+        children: [
+          TableRow(
+            children: [
+              Container(
+                height: 10,
+                width: 10,
+                color: Colors.orange,
+              ),
+              Container(
+                height: 30,
+                width: 10,
+                color: Colors.orange,
+              )
+            ],
+          ),
+          TableRow(children: [
+            Container(
+              height: 10,
+              width: 20,
+              color: Colors.orange,
+            ),
+            Container(
+              height: 5,
+              width: 5,
+              color: Colors.orange,
+            )
+          ])
+        ],
+      ),
+    ),
+    ColoredBox(
+      color: Colors.black,
+      child: Transform(
+        alignment: Alignment.topRight,
+        transform: Matrix4.skewY(0.3)..rotateZ(-math.pi / 12.0),
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          color: const Color(0xFFE8581C),
+          child: const Text('Este é o Transform!!!'),
+        ),
+      ),
+    ),
+    Container(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,24 +97,15 @@ class TesteWidgetsPageState extends State<TesteWidgetsPage> {
         title: const Text("Teste dos Widgets"),
       ),
       body: GridView.builder(
-        itemCount: 10,
+        itemCount: widgets.length,
         gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (gridContext, gridIndex) {
+          print(widgets.length);
           return LayoutBuilder(
             builder: (l2Context, l2Constraints) {
               return Container(
-                // Define o tamanho do Container
-                width: 80, // Ajuste os valores
-                height: 40, // Ajuste os valores
-                // Centraliza o ElevatedButton
-                alignment: Alignment.center,
-                child: ElevatedButton(
-                  onPressed: () {
-                    print(l2Constraints);
-                  },
-                  child: Text("Botão"),
-                ),
+                child: widgets[gridIndex],
               );
             },
           );
